466247486844c8351c1541d35081f4af33857e65
Cyber-Dragon2 <amritsingh1425@gmail.com>
2023-10-12 22:53:54 +0530

    edited fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,23 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void fibonacci(int n){
-	int prev2 = 0, prev1 = 1;
-	printf("%d %d ", prev2, prev1);
-	for (int i = 2; i < n; i++)
-	{
-		int current = prev1 + prev2;
-		printf("%d ", current);
-		prev2 = prev1;
-		prev1 = current;
-	}
-	printf("\n");
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int main(int argc, char const *argv[]){
-	int n;
-	printf("Enter number: ");
-	scanf("%d", &n);
-	printf("Fibonacci series of %d digits: ", n);
-	fibonacci(n);
-	return 0;
+int main() {
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series of %d terms: ", n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+	printf("\n");
+	return 0;	
 }
